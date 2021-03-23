06be17a9eaead98990221426d0120cf0515de044
Amy Gori <amydgori@gmail.com>
2021-03-22 21:17:03 -0400

    Add ability to select a country in the list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CountryList from './components/CountryList'
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     axios.get('https://api.covid19api.com/countries').then((response) => {
@@ -19,7 +20,10 @@ function App() {
   return (
     <div className="App mw8 center mv3 ph3 sans-serif">
       <h1 className="pa2 bg-orange white">COVID-19 Data Explorer</h1>
-      <CountryList countries={countries} />
+      <CountryList
+        countries={countries}
+        setSelectedCountry={setSelectedCountry}
+      />
     </div>
   )
 }
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,11 +1,16 @@
-export default function CountryList({ countries }) {
+export default function CountryList({ countries, setSelectedCountry }) {
   return (
     <div>
       <h2>List of countries</h2>
       <ul className="list pl0">
         {countries.map((country) => (
           <li className="mb2 mr3 dib" key={country.Slug}>
-            {country.Country}
+            <button
+              className="pa0 bw0 bg-white blue pointer underline-hover"
+              onClick={() => setSelectedCountry(country)}
+            >
+              {country.Country}
+            </button>
           </li>
         ))}
       </ul>
